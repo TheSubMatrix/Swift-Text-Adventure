48cdcc5e423accb063443a324ea4c9d2ff9d812d
Colin Whiteford <colinwhiteford516@gmail.com>
2025-08-28 16:07:23 -0400

    Finished Project

--- Text Adventure/Text Adventure/main.swift
+++ Text Adventure/Text Adventure/main.swift
@@ -26,14 +26,27 @@ class Inventory
             if(items[i] == itemToRemove)
             {
                 items.remove(at: i)
+                break;
             }
         }
+    }
+    func CheckInventoryWinCondition(itemsToWin: Array<Item>) -> Bool
+    {
+        var result = true;
+        for i in itemsToWin
+        {
+            if !items.contains(i)
+            {
+                result = false;
+            }
+        }
+        return result;
     }
 }
 
 class Room
 {
-    init(roomName: String, connectedRooms: Array<Room>, itemsInRoom: Array<Item>, challenge: Challenge)
+    init(roomName: String, connectedRooms: Array<Room>, itemsInRoom: Array<Item>, challenge: Challenge?)
     {
         self.connectedRooms = connectedRooms;
         self.itemsInRoom = itemsInRoom;
@@ -43,7 +56,7 @@ class Room
     var roomName:String
     var connectedRooms: Array<Room>
     var itemsInRoom: Array<Item>
-    var challengeInRoom: Challenge
+    var challengeInRoom: Challenge?
 }
 struct Challenge
 {
@@ -60,12 +73,34 @@ func ParsePlayerCommand()
     {
         let userCommand = readLine()
         let splitCommand: Array<String> = (userCommand?.components(separatedBy: " "))!
-        let command = splitCommand[0]
+        let command = splitCommand[0].lowercased()
+        var splitCommandParams = Array<String>(splitCommand)
+        splitCommandParams.removeFirst()
+        let commandParams = splitCommandParams.joined()
         switch command
         {
         case "go":
-            print("going up")
-            inputValid = true
+            if(TravelCommand(playerInput: commandParams))
+            {
+                print("Traveled to " + commandParams)
+                inputValid = true
+            }
+            else
+            {
+                print("Not a valid location")
+            }
+        case "pickup":
+            if PickupCommand(playerInput: commandParams)
+            {
+                print("Picked up " + commandParams)
+                inputValid = true
+            }
+            else
+            {
+                print("Not a valid item")
+            }
+        case "inventory":
+            InventoryCommand()
         default:
             print("Invalid command, please try again")
         }
@@ -81,13 +116,128 @@ struct Item: Equatable
     var name:String
 }
 
+func TravelCommand(playerInput: String) -> Bool
+{
+    if let roomToTravelTo = currentRoom.connectedRooms.first(where: {(s1: Room) -> Bool in return playerInput != "" && s1.roomName.lowercased() == playerInput.lowercased()})
+    {
+        currentRoom = roomToTravelTo;
+        return true;
+    }
+    return false;
+}
+func PickupCommand(playerInput: String) -> Bool
+{
+    if let itemToPickup = currentRoom.itemsInRoom.first(where: {(s1: Item) -> Bool in return playerInput != "" && s1.name.lowercased() == playerInput.lowercased()})
+    {
+        playerInventory.AddToInventory(itemToAdd: itemToPickup)
+        if playerInventory.items.count == 0 {return false;}
+        for i in 0...currentRoom.itemsInRoom.count-1
+        {
+            if(currentRoom.itemsInRoom[i] == itemToPickup)
+            {
+                currentRoom.itemsInRoom.remove(at: i)
+                break;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+func InventoryCommand()
+{
+    if(playerInventory.items.count > 0)
+    {
+        for i in 0...playerInventory.items.count-1
+        {
+            var suffix = ", "
+            if(i >= playerInventory.items.count-1)
+            {
+                suffix = ""
+            }
+            print(playerInventory.items[i].name + suffix , terminator: "")
+        }
+        print("")
+    }else
+    {
+        print("Empty")
+    }
+}
+
+func PrintCurrentGameState()
+{
+    print("You are at the " + currentRoom.roomName + ".")
+    print("You see ", terminator: "")
+    if(currentRoom.itemsInRoom.count > 0)
+    {
+        for i in 0...currentRoom.itemsInRoom.count-1
+        {
+            var suffix = ", "
+            if(i >= currentRoom.itemsInRoom.count-1)
+            {
+                suffix = ""
+            }
+            print(currentRoom.itemsInRoom[i].name + suffix , terminator: "")
+        }
+        print("")
+    }
+    else
+    {
+        print("nothing")
+    }
+    print("You can go to ", terminator: "")
+    for i in 0...currentRoom.connectedRooms.count-1
+    {
+        var suffix = ", "
+        if(i >= currentRoom.connectedRooms.count-1)
+        {
+            suffix = ""
+        }
+        print(currentRoom.connectedRooms[i].roomName + suffix, terminator: "")
+    }
+    print("")
+    
+}
 
 //======Main Body======
 var gameOver = false
-var playerInventory: Inventory
-var startingRoom: Room
+var playerInventory = Inventory()
 
+var crown = Item(name: "Crown")
+var bracelet = Item(name: "Bracelet")
+var necklace = Item(name: "Necklace")
+var ring = Item(name: "Ring")
+var earrings = Item(name: "Earrings")
+
+var startingRoom = Room(roomName: "Entrance", connectedRooms: Array<Room>(), itemsInRoom: Array<Item>(), challenge: nil)
+var northRoom = Room(roomName: "North", connectedRooms: Array<Room>(), itemsInRoom: Array<Item>(), challenge: nil)
+var southRoom = Room(roomName: "South", connectedRooms: Array<Room>(), itemsInRoom: Array<Item>(), challenge: nil)
+var eastRoom = Room(roomName: "East", connectedRooms: Array<Room>(), itemsInRoom: Array<Item>(), challenge: nil)
+var westRoom = Room(roomName: "West", connectedRooms: Array<Room>(), itemsInRoom: Array<Item>(), challenge: nil)
+
+northRoom.connectedRooms.append(startingRoom)
+southRoom.connectedRooms.append(startingRoom)
+eastRoom.connectedRooms.append(startingRoom)
+westRoom.connectedRooms.append(startingRoom)
+startingRoom.connectedRooms.append(northRoom)
+startingRoom.connectedRooms.append(southRoom)
+startingRoom.connectedRooms.append(eastRoom)
+startingRoom.connectedRooms.append(westRoom)
+
+startingRoom.itemsInRoom.append(crown)
+northRoom.itemsInRoom.append(bracelet)
+southRoom.itemsInRoom.append(necklace)
+eastRoom.itemsInRoom.append(ring)
+westRoom.itemsInRoom.append(earrings)
+
+var currentRoom = startingRoom
 while(!gameOver)
 {
+    PrintCurrentGameState()
     ParsePlayerCommand()
+    if(playerInventory.CheckInventoryWinCondition(itemsToWin: [crown, bracelet, necklace, ring, earrings]))
+    {
+        print("You win!")
+        gameOver = true
+    }
 }
